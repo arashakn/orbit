27eeb190c8b0ee7351e0d804a6282b58ca962fe9
Arash Alikhani <arash@tmpadmins-MacBook-Pro-2.local>
2017-05-15 15:31:27 -0700

    First commit

---
+++ core/src/orbital/OrbitalsGame.java
@@ -0,0 +1,29 @@
+package orbital;
+
+
+import com.badlogic.gdx.Game;
+
+import helpers.AssetLoader;
+import screens.SplashScreen;
+
+public class OrbitalsGame extends Game {
+
+    private ActionResolver actionresolver;
+
+
+    public OrbitalsGame(ActionResolver actionresolver) {
+        this.actionresolver = actionresolver;
+    }
+
+    @Override
+    public void create() {
+        AssetLoader.load();
+        setScreen(new SplashScreen(this, actionresolver));
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        AssetLoader.dispose();
+    }
+}
